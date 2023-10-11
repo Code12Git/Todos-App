c8b440f81399be22f4690c3f3b542ad05336eb68
Code12Git <saxenasaksham46@gmail.com>
2023-10-11 19:50:18 +0530

    Added Sorting

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,14 +3,11 @@ import {
 	createTodo,
 	deleteTodo,
 	getAllTodo,
-	searchTodo,
 	updateTodo,
 	statusChangeController,
 	prioritizedTodo,
 	updatePriority,
 	Pagination,
-	sortTodoByDate,
-	// sortTodoByPriority,
 } from "../controllers/todoController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -25,20 +22,8 @@ router.put("/:id", verifyToken, updateTodo);
 //Delete Todo
 router.delete("/:id", verifyToken, deleteTodo);
 
-//Get Todo
-// router.get("/:id", verifyToken, getTodo);
-
 //Get All Todo
 router.get("/", verifyToken, getAllTodo);
-
-//Search Todo
-router.get("/search", verifyToken, searchTodo);
-
-//Sort Todo
-// router.get("/sort", verifyToken, sortTodoByPriority);
-
-//Sort todo on basis of date
-router.get("/sort/date", verifyToken, sortTodoByDate);
 
 //Status Change
 router.put("/status/:id", verifyToken, statusChangeController);
